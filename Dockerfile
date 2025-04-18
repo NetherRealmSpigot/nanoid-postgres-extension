b622842af709191fa076a7d72e2db20f4224d85e
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-04-18 23:53:52 +0800

    ADD

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+ARG BASE_IMAGE=postgres:16-alpine
+FROM $BASE_IMAGE
+
+COPY . /tmp/ext
+
+RUN <<EOS
+apk --no-cache add build-base postgresql16-dev clang16 llvm16 ca-certificates openssl tar bash git
+cd /tmp/ext
+chmod +x ./install.sh
+bash ./install.sh
+apk del build-base postgresql16-dev clang16 llvm16 ca-certificates openssl tar bash git
+apk del .fetch-deps .build-deps
+rm -rf /tmp/ext
+EOS
--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-export WORKING_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WORKING_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+cd $WORKING_DIR
 
 set -ex
 set -o pipefail
 
+git submodule update --init --recursive
 export SQL_INSTALL_SCRIPT=nanoid--1.0.sql
 rm -rf "$WORKING_DIR/$SQL_INSTALL_SCRIPT"
 cp $WORKING_DIR/nanoid-postgres/nanoid.sql "$WORKING_DIR/$SQL_INSTALL_SCRIPT"
